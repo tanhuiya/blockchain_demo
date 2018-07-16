fa95ed616ef9c5190b62044ba033f5a7b24f298b
tanhui <tanhui@corp-ci.com>
2018-07-16 17:56:47 +0800

    day2

---
+++ block.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"time"
+)
+
+type Block struct {
+	Timestamp		int64
+	PrevBlockHash 	[]byte
+	Hash			[]byte
+	Data 			[]byte
+	Nonce 			int
+}
+
+func NewBlock(data string, preBlockHash []byte) *Block {
+	block := &Block{
+		Timestamp: time.Now().Unix(),
+		PrevBlockHash: preBlockHash,
+		Hash: []byte{},
+		Data: []byte(data),
+		Nonce: 0,
+	}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+	block.Hash = hash[:]
+	block.Nonce = nonce
+	return block
+}
+
+
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
---
+++ utils.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"encoding/binary"
+	"bytes"
+)
+
+func IntToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+	return buff.Bytes()
+}
